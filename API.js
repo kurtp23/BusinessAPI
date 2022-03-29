1af4fb2c4342cdcaa3d9474a372ac2ddf84671c8
kurtp23 <kurt_phillips@live.com>
2022-03-28 18:23:56 -0700

    first api

---
+++ API.js
@@ -0,0 +1,21 @@
+const https = require("https");
+
+https
+  .get("https://data.oregon.gov/resource/esjy-u4fc.json", (resp) => {
+    let data = "";
+
+    // A chunk of data has been received.
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  })
+  .on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+//file IO / json - blank text  / csv file
